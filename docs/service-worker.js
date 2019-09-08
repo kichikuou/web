4949796181b04da50c3e54505e7cc0c55d0c8cb0
kichikuou <KichikuouChrome@gmail.com>
2019-09-08 09:28:03 +0900

    Update service worker
    - Workbox 4.3.1
    - Use stale-while-revalidate for unversioned resources

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 let cacheOptions = {
     cacheName: 'kichikuou',
@@ -13,5 +13,5 @@ let cacheOptions = {
 // Cache-first for fonts, versioned libraries, and sound patches
 workbox.routing.registerRoute(/\/(fonts|lib|Tone_000|Drum_000)\//, new workbox.strategies.CacheFirst(cacheOptions));
 
-// Network-first for other same-origin resources
-workbox.routing.registerRoute(/\//, new workbox.strategies.NetworkFirst(cacheOptions));
+// Stale-while-revalidate for other same-origin resources
+workbox.routing.registerRoute(/\//, new workbox.strategies.StaleWhileRevalidate(cacheOptions));
