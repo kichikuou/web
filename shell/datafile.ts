3666ad9a90a00906330819c614002f73d9c78674
kichikuou <KichikuouChrome@gmail.com>
2024-11-28 10:16:46 +0900

    Remove mmap hack
    It doesn't work with Emscripten 3.1.65 and later.

--- shell/datafile.ts
+++ shell/datafile.ts
@@ -2,9 +2,6 @@
 // This source code is governed by the MIT License, see the LICENSE file.
 import {urlParams} from './util.js';
 
-const major = 240;
-let entryCount = 0;
-
 type PatchTable = [number, number, number][]; // addr, old, new
 
 // See xsystem35-sdl2/patch/README.TXT
@@ -21,92 +18,35 @@ const TADAModePatch: PatchTable = [
 ];
 
 export function registerDataFile(fname: string, size: number, chunks: Uint8Array[]) {
-    let patch: PatchTable | null = null;;
-    switch (fname.toUpperCase()) {
-    case 'ぱすてるSA.ALD':
-        patch = PastelChimePatch;
-        break;
-    case '鬼畜王SA.ALD':
-        if (urlParams.get('tada') === '1')
-            patch = TADAModePatch;
-        break;
-    }
-
     const path = '/' + fname;
-    if (fname.match(/\.(ald|ain|alk|kld)$/i)) {
-        // Put the file content in the WASM memory space, so that it won't
-        // consume additional heap space when mmaped.
-        const dev = Module!.FS.makedev(major, entryCount++);
-        const ops = new NodeOps(size, chunks, patch);
-        Module!.FS.registerDevice(dev, ops);
-        Module!.FS.mkdev(path, dev, undefined);
-    } else {
-        // Store as a regular file.
-        var f = Module!.FS.open(path, 'w', undefined);
-        for (const chunk of chunks) {
-            Module!.FS.write(f, chunk, 0, chunk.byteLength, undefined, undefined);
-        }
-        Module!.FS.close(f);
+    var f = Module!.FS.open(path, 'w+', undefined);
+    for (const chunk of chunks) {
+        Module!.FS.write(f, chunk, 0, chunk.byteLength, undefined, undefined);
     }
+    switch (fname.toUpperCase()) {
+        case 'ぱすてるSA.ALD':
+            apply_patch(f, PastelChimePatch);
+            break;
+        case '鬼畜王SA.ALD':
+            if (urlParams.get('tada') === '1')
+                apply_patch(f, TADAModePatch);
+            break;
+    }
+    Module!.FS.close(f);
 }
 
-class NodeOps {
-    private addr: number | undefined;
-    private chunks: Uint8Array[] | null
-
-    constructor(private size: number, chunks: Uint8Array[], private patchTbl: PatchTable | null) {
-        this.chunks = chunks;
-    }
-
-    read(stream: any, buffer: Int8Array, offset: number, length: number, position: number): number {
-        if (buffer !== Module!.HEAP8)
-            throw new Error('Invalid argument');
-        if (this.addr === undefined)
-            this.load();
-        let src = this.addr! + position;
-        length = Math.min(length, this.size - position);
-        // load() might have invalidated `buffer`, so use Module.HEAP8 directly
-        Module!.HEAP8.set(Module!.HEAPU8.subarray(src, src + length), offset);
-        return length;
-    }
-
-    llseek(stream: any, offset: number, whence: number): number {
-        let position = offset;
-        if (whence === 1)  // SEEK_CUR
-            position += stream.position;
-        else if (whence === 2)  // SEEK_END
-            position += this.size;
-        return position;
-    }
-
-    mmap() {
-        if (this.addr === undefined)
-            this.load();
-        return { ptr: this.addr, allocated: false };
-    }
-
-    private load() {
-        let ptr = this.addr = Module!._malloc(this.size);
-        for (let c of this.chunks!) {
-            Module!.HEAPU8.set(c, ptr);
-            ptr += c.byteLength;
-        }
-        this.chunks = null;
-        this.patch();
-    }
-
-    private patch() {
-        if (!this.patchTbl)
+function apply_patch(f: any, patchTbl: PatchTable) {
+    const buf = new Uint8Array(1);
+    for (let a of patchTbl) {
+        Module!.FS.read(f, buf, 0, 1, a[0]);
+        if (buf[0] !== a[1]) {
+            console.log('Patch failed');
             return;
-        for (let a of this.patchTbl) {
-            if (Module!.HEAPU8[this.addr! + a[0]] !== a[1]) {
-                console.log('Patch failed');
-                return;
-            }
         }
-        for (let a of this.patchTbl)
-            Module!.HEAPU8[this.addr! + a[0]] = a[2];
-        console.log('Patch applied');
-        this.patchTbl = null;
     }
+    for (let a of patchTbl) {
+        buf[0] = a[2];
+        Module!.FS.write(f, buf, 0, 1, a[0], undefined);
+    }
+    console.log('Patch applied');
 }
