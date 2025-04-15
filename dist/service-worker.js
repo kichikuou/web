2e970b540e92b7fbbf13b1c43327f1b4408e81e5
kichikuou <KichikuouChrome@gmail.com>
2025-04-16 08:08:47 +0900

    ServiceWorker: Use network-first for most resources

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -10,11 +10,8 @@ let cacheOptions = {
     ],
 };
 
-// Cache-first for fonts and sound patches.
-workbox.routing.registerRoute(/\/(fonts|Tone_000|Drum_000)\//, new workbox.strategies.CacheFirst(cacheOptions));
+// Cache-first for fonts and SoundFonts.
+workbox.routing.registerRoute(/\/(fonts|soundfonts)\//, new workbox.strategies.CacheFirst(cacheOptions));
 
-// Network first for the game engines, shell.js and help pages.
-workbox.routing.registerRoute(/\/(shell.js|(system3|xsystem35)\.(js|wasm)|help\/)/, new workbox.strategies.NetworkFirst(cacheOptions));
-
-// Stale-while-revalidate for other same-origin resources
-workbox.routing.registerRoute(/\//, new workbox.strategies.StaleWhileRevalidate(cacheOptions));
+// Network first for other same-origin resources.
+workbox.routing.registerRoute(/\//, new workbox.strategies.NetworkFirst(cacheOptions));
